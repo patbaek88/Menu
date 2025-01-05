7fcfddf63876e3bdb193ac47a83dc44b801b439b
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-01-05 11:51:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.dataframe(df)
 price_min = st.slider('최소 가격', 0, 100000, 0)
 price_max = st.slider('최대 가격', 1000, 100000, 20000)
 delivery = st.checkbox('배달음식만')
-hunger = st.checkbox('배부른음식만')
+hunger = st.checkbox('배 안부른 음식만')
 materials = ['pork','beef', 'chicken', 'shrimp', 'golbaengi','fish', 'clam', 'flower', 'kimchi', 'potato']
 main_material = st.multiselect('주재료', materials, default = materials)
 spicy = st.checkbox('매콤')
@@ -20,7 +20,7 @@ oily = st.checkbox('기름진')
 time_max = st.slider('최대 소요시간', 5,120, 30)
 prefer = st.radio('선호자', ['Jaeuk', 'Heeeun', 'both'])
 
-filtered_df = df[(df["unit price"] >= price_min) & (df["unit price"] <= price_max) & (df["delivery"] >= (1 if delivery else 0)) & (df["hungry"] >= (1 if hunger else 0)) & (df["main ingredient"].apply(lambda x: any(material in x for material in main_material))) & (df["spicy"] >= (1 if spicy else 0)) & (df["sweet"] >= (1 if sweet else 0)) & (df["salty"] >= (1 if salty else 0)) & (df["oily"] >= (1 if oily else 0)) & (df["time"] <= time_max) &(df["prefer"] == prefer)]
+filtered_df = df[(df["unit price"] >= price_min) & (df["unit price"] <= price_max) & (df["delivery"] >= (1 if delivery else 0)) & (df["hungry"] <= (0 if hunger else 1)) & (df["main ingredient"].apply(lambda x: any(material in x for material in main_material))) & (df["spicy"] >= (1 if spicy else 0)) & (df["sweet"] >= (1 if sweet else 0)) & (df["salty"] >= (1 if salty else 0)) & (df["oily"] >= (1 if oily else 0)) & (df["time"] <= time_max) &(df["prefer"] == prefer)]
 
 st.write("조건에 맞는 메뉴")
 st.write(filtered_df["menu"].tolist())
